d05590fcbab61da271edd7083480cf3cbfafb549
Kirill Rasuli <rasulikv@gmail.com>
2020-04-14 22:38:40 +0300

    Class-based view for list and pagination added

--- myblog/views.py
+++ myblog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')  # current page number
+    try:
+        posts = paginator.page(page)  # list of objects on the current page
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
@@ -12,5 +29,4 @@ def post_detail(request, year, month, day, post):
         Post, slug=post, status='published',
         publish__year=year, publish__month=month, publish__day=day
     )
-    return render(request, 'blog/post/detail.html', {'post': post})
-    
+    return render(request, 'blog/post/detail.html', {'post': post}) 
