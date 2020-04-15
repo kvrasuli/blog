8461f663080029b8a56f81b5faa763da9bc2ab10
Kirill Rasuli <rasulikv@gmail.com>
2020-04-15 17:24:22 +0300

    Comments subsystem implemented

--- myblog/models.py
+++ myblog/models.py
@@ -31,7 +31,6 @@ class Post(models.Model):
     objects = models.Manager()  # default manager
     published = PublishedManager()  # our new manager
 
-
     class Meta:
         ordering = ('-publish',)
 
@@ -49,3 +48,17 @@ class Post(models.Model):
                 ])
 
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
--- myblog/views.py
+++ myblog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 
 
@@ -31,7 +31,27 @@ def post_detail(request, year, month, day, post):
         Post, slug=post, status='published',
         publish__year=year, publish__month=month, publish__day=day
     )
-    return render(request, 'blog/post/detail.html', {'post': post}) 
+    # список активных комментариев для этой статьи
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        # пользователь отправил комментарий
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # создаем комментарий, но пока не сохраняем в БД
+            new_comment = comment_form.save(commit=False)
+            # привязываем коммент к текущей статье
+            new_comment.post = post
+            # сохраняем коммент в БД
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request, 'blog/post/detail.html', {
+        'post': post,
+        'comments': comments,
+        'new_comment': new_comment,
+        'comment_form': comment_form
+        })
 
 
 def post_share(request, post_id):
