28a6c04e0f8f6327cd59449e18aa2a5c85ca41e7
Kirill Rasuli <rasulikv@gmail.com>
2020-04-15 15:35:10 +0300

    Post sharing by email is implemented

--- myblog/views.py
+++ myblog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 
 class PostListView(ListView):
@@ -30,3 +32,23 @@ def post_detail(request, year, month, day, post):
         publish__year=year, publish__month=month, publish__day=day
     )
     return render(request, 'blog/post/detail.html', {'post': post}) 
+
+
+def post_share(request, post_id):
+    # получение статьи по идентификатору
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    if request.method == 'POST':
+        # форма была отправлена на сохранение
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            # отправка электронной почты
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} ({cd['email']}) recommends you reading\"{post.title}\""
+            message = f"Read \"{post.title}\" at {post_url}\n\n{cd['name']}\'s comments:\n{cd['comments']}"
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
